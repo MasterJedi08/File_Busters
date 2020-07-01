958783b27803148645bcd9ba9433c0fa90db43a3
Ashley <ashley.jablonski@students.loy.org>
2020-07-01 12:43:10 -0500

    12:41PM CST update nn.py
    update before we fix list of strings

--- nn.py
+++ nn.py
@@ -76,58 +76,17 @@ def load_email(is_spam, filename):
 ham_emails = [load_email(is_spam=False, filename=name) for name in ham_filenames]
 spam_emails = [load_email(is_spam=True, filename=name) for name in spam_filenames]
 
-print(type(ham_emails[0]))    
-
-from collections import Counter
-
-def get_email_structure(email):
-    if isinstance(email, str):
-        return email
-    payload = email.get_payload()
-    if isinstance(payload, list):
-        return "multipart({})".format(", ".join([
-            get_email_structure(sub_email)
-            for sub_email in payload
-        ]))
-    else:
-        return email.get_content_type()
-
-def structures_counter(emails):
-    structures = Counter()
-    for email in emails:
-        structure = get_email_structure(email)
-        structures[structure] += 1
-    return structures
-
-ham_structure = structures_counter(ham_emails)
-spam_structure = structures_counter(spam_emails)
-
-# keep this one??
-def html_to_plain(email):
-    try:
-        soup = BeautifulSoup(email.get_content(), 'html.parser')
-        return soup.text.replace('\n\n','')
-    except:
-        return "empty"
-
-def email_to_plain(email):
-    struct = get_email_structure(email)
-    for part in email.walk():
-        partContentType = part.get_content_type()
-        if partContentType not in ['text/plain','text/html']:
-            continue
-        try:
-            partContent = part.get_content()
-        except: # in case of encoding issues
-            partContent = str(part.get_payload())
-        if partContentType == 'text/plain':
-            return partContent
+# from stack overflow idk if this works?? returns TypeError: argument of type 'NoneType' is not iterable
+for i in range(0, len(ham_emails)):
+    message = ham_emails[i]
+    if 'parts' in message['payload']:
+        if message['payload']['parts'][0]['mimeType'] == 'multipart/alternative':
+            ham_emails[i] = message['payload']['parts'][0]['parts'][0]['body']['data']    
         else:
-            return html_to_plain(part)
+            ham_emails[i] = message['payload']['parts'][0]['body']['data']   
+    else:
+        ham_emails[i] = message['payload']['body']['data']
 
-print(email_to_plain(ham_emails[42]))
-print(email_to_plain(spam_emails[42]))
-    
 #joey
 numTrainHam = round(len(ham_emails)*.7,0)
 numTestHam = len(ham_emails) - numTrainHam
@@ -139,18 +98,21 @@ testHam = []
 testSpam = []
 trainHam = []
 trainSpam = []
-for i in range(len(ham_emails)):
-    if i < numTrainHam:
-        trainHam.append(ham_emails[i-1])
+for i in range(0, len(ham_emails)-1):
+    temp = ham_emails[i].get_content()
+    if i < numTrainHam:        
+        trainHam.append(temp)
     else:
-        testHam.append(ham_emails[i-1])
-for i in range(len(spam_emails)):
-    if i < numTrainSpam:
-        trainSpam.append(spam_emails[i-1])
-    else:
-        testSpam.append(spam_emails[i-1])
+        testHam.append(temp)
 
-#endJOey
+for j in range(0, len(spam_emails)-1):
+    temp = spam_emails[j].get_content()
+    if j < numTrainSpam:        
+        trainSpam.append(temp)
+    else:
+        testSpam.append(temp)
+
+# endJOey
 
 #train_emails
 train_emails = []
