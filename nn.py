a5b3fcc5b46ef47d0a8ea8bbe0c004be2139c5ce
Ashley <ashley.jablonski@students.loy.org>
2020-07-02 14:49:35 -0500

    deleted new_nn.py and created the new nn.py

--- nn.py
+++ nn.py
@@ -12,44 +12,18 @@ import email
 import email.policy
 from bs4 import BeautifulSoup
 import logging
-<<<<<<< Updated upstream
-
-### ASHLEY what do these lines do?
-=======
 import clean
->>>>>>> Stashed changes
+
+# logging - debug statements throughout code
 logging.basicConfig(filename='debug6.txt', level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
 logging.debug('Start of program')
-
-# Loading the dataset
-os.listdir('C:\\Users\\Student\\Desktop\\extension_data\\hamnspam') #finding the location of the data on the desktop
-
-#creating the lists of information for both the ham (non spam) and spam information
-ham_filenames = [name for name in sorted(os.listdir('C:\\Users\\Student\\Desktop\\extension_data\\hamnspam\\ham')) if len(name) > 20]
-spam_filenames = [name for name in sorted(os.listdir('C:\\Users\\Student\\Desktop\\extension_data\\hamnspam\\spam')) if len(name) > 20]
-
-#loads the data from the directory and opens the files 
-def load_email(is_spam, filename):
-    directory = "C:\\Users\\Student\\Desktop\\extension_data\\hamnspam\\spam" if is_spam else "C:\\Users\\Student\\Desktop\\extension_data\\hamnspam\\ham"
-    with open(os.path.join(directory, filename), "rb") as f:
-        return email.parser.BytesParser(policy=email.policy.default).parse(f)
-
-# making lists to match index values with filenames    
-ham_emails = [load_email(is_spam=False, filename=name) for name in ham_filenames]
-spam_emails = [load_email(is_spam=True, filename=name) for name in spam_filenames]
-
-x = ham_emails[0].get_content()
-y = type(x)
-logging.debug('ham_emails[0].get_content = %s' % (x))
-logging.debug('type of ham_emails[0].get_content = %s' % (y))
+# disables all logging statements
+logging.disable()
 
 # CONSTANT VARIABLES
 NUM_EPOCHS = 5
 FILENAME = "file_busters_model.h5"
 
-<<<<<<< Updated upstream
-# function used to train the data
-=======
 def preprocess():
     # Load dataset
     # location test data
@@ -132,7 +106,6 @@ def preprocess():
     for x in range(trainSpam):
         train_labels.append("spam")   
 
-
     #train_labels 
     test_labels = []
     for x in range(testHam):
@@ -141,29 +114,20 @@ def preprocess():
     for x in range(testSpam):
         test_labels.append("spam")   
 
-    # logging.debug('converting into numpy arrays')
-    # conversion from lists to numPy array (lists not supported by tensor)
-    # train_emails_arrays = np.asarray(train_emails)
-    # train_labels_arrays = np.asarray(train_labels)
-    # test_emails_arrays = np.asarray(test_emails)
-    # test_labels_arrays = np.asarray(test_labels)
-
-    # convert_to_tensor()
-    train_emails, train_labels, test_emails, test_labels = clean.clean_data(train_emails, train_labels, test_emails, test_labels)
+    # cleans up email data
+    train_emails, test_emails = clean.clean_data(train_emails, test_emails)
     print(train_emails[0:2])
     print(train_labels[0:5])
     return train_emails, train_labels, test_emails, test_labels
 
-    
-
-# train_img = [ham + spam] train_labels = [ham_label + spam_label] << index must match
->>>>>>> Stashed changes
 def train(train_emails, train_labels):
     # try to load already saved model
     try:
         # this line will throw error if file doesn't exist
         model=tf.keras.models.load_model(FILENAME)
     except:
+        train_emails = tf.convert_to_tensor(train_emails)
+        train_labels = tf.convert_to_tensor(train_labels)
         #create model
         model = keras.Sequential([ # Sequential means sequence of layers
             # 128 neurons, rectified linear unit
@@ -184,6 +148,7 @@ def train(train_emails, train_labels):
 
     return model
 
+    
 # checks to see if the model is able to correctly predict the type (spam/ham) of email
 # with un-seen data
 def predict(model, test_images, test_labels):
@@ -191,7 +156,7 @@ def predict(model, test_images, test_labels):
     # params: test input and output
     # returns loss and accuracy -> wrong vs right
     test_loss, test_accuracy = model.evaluate(test_images, test_labels) 
-    print("Accuracy", test_accuracy)
+    print("Accuracy", test_accuracy, "\nLoss", test_loss)
 
     # use test data to predict output
     predictions = model.predict(test_images[:10])
@@ -199,112 +164,14 @@ def predict(model, test_images, test_labels):
         #print prediction and actual
         print("Actual:", test_labels[i], "Expected:", np.argmax(predictions[i]))
 
-<<<<<<< Updated upstream
-#creating the testing group vs. the training group
-numTrainHam = round(len(ham_emails)*.8,0)
-numTestHam = len(ham_emails) - numTrainHam
-numTrainSpam = round(len(spam_emails)*.8,0)
-numTestSpam = len(spam_emails) - numTrainHam
-
-# creating lists for the content of spam vs ham emails
-testHam = []
-testSpam = []
-trainHam = []
-trainSpam = []
-
-# for loop that cycles through the ham emails to separate them into testing and training data
-logging.debug('Entering ham for loop of adding data to test/train lists')  #statement helps us keep track of where we are in the process via the terminal
-
-for i in range(0, len(ham_emails)-1):
-    # temp = ham_emails[i].get_content()
-    msg = ham_emails[i]
-    logging.debug("Index: %d" %(i))
-    logging.debug('Entering ham msg walking for loop')
-    for part in msg.walk():
-        # each part is a either non-multipart, or another multipart message
-        # that contains further parts... Message is organized like a tree
-        if part.get_content_type() == 'text/plain':
-            temp = part.get_payload()
-    z = type(temp)
-    logging.debug("Current email: %s" %(z))
-    if i < numTrainHam:        
-        trainHam.append(temp)    
-    else:
-        testHam.append(temp)
-
-# for loop that cycles through the apam emails to separate them into testing and training data
-logging.debug('Finished ham - Entering spam for loop of adding data to test/train lists')  #statement helps us keep track of where we are in the process via the terminal
-
-for j in range(0, len(spam_emails)-1):
-    # temp = spam_emails[j].get_content()
-    msg = ham_emails[i]
-    logging.debug("Index: %d" %(j))
-    logging.debug('Entering spam msg walking for loop')
-    for part in msg.walk():
-        # each part is a either non-multipart, or another multipart message
-        # that contains further parts... Message is organized like a tree
-        if part.get_content_type() == 'text/plain':
-            temp = part.get_payload()
-    if j < numTrainSpam:        
-        trainSpam.append(temp)
-    else:
-        testSpam.append(temp)
-
-#shows that all data has been sorted appropriately into testing/training data
-logging.debug('Finished spam')
-
-# this list contains all of the emails used for training (both ham and spam)
-train_emails
-train_emails = []
-train_emails.append(trainHam)
-train_emails.append(trainSpam)
-
-# this list assigns the appropriate names to the type of email contained in the list above
-#     meaning that for however many ham emails are in the list above, the name "ham" appears in this list to match the same indecies
-# used for training the data
-train_labels = []
-for x in range(len(trainHam)):
-    train_labels.append("ham")
-
-for x in range(len(trainSpam)):
-    train_labels.append("spam")   
-
-# this list contains all of the emails used for testing (both ham and spam) 
-test_emails = []
-test_emails.append(testHam)
-test_emails.append(testSpam)
-
-# this list assigns the appropriate names to the type of email contained in the list above
-#     meaning that for however many ham emails are in the list above, the name "ham" appears in this list to match the same indecies
-# used for testing the data
-test_labels = []
-for x in range(len(testHam)):
-    test_labels.append("ham")
-
-for x in range(len(testSpam)):
-    test_labels.append("spam")   
-
-logging.debug('converting into numpy arrays')
-# conversion from lists to numPy array (lists not supported by tensor)
-train_emails_arrays = np.asarray(train_emails)
-train_labels_arrays = np.asarray(train_labels)
-test_emails_arrays = np.asarray(test_emails)
-test_labels_arrays = np.asarray(test_labels)
-
-logging.debug('going into training')
-
-#train the data
-train(train_emails_arrays, train_labels_arrays)
-
-=======
+# preprocess data
 train_emails, train_labels, test_emails, test_labels = preprocess()
 logging.debug('going into training')
 # train the data
 train(train_emails, train_labels)
 logging.debug('going into testing')
->>>>>>> Stashed changes
 # test model
 logging.debug('going into testing')
-predict(model, test_emails_arrays, test_labels_arrays)
+predict(model, test_emails, test_labels)
 logging.debug('End Program') #signifies the end of the program through the terminal
-# ugh# ugh
+
